122b5cfed1d0880624c216045c698462d2f0d014
Eva Singh <eva.singh@hotmail.com>
2020-12-09 20:12:33 +0100

    Add router and navigation

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import HomePage from "../home/HomePage";
+import ContactPage from "../contact/ContactPage";
+
+function Layout({children}) {
+    return(
+      <Router>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="/">React CA</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavLink to="/" exact className="nav-link">Home</NavLink>
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <Container>
+          <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/contact" component={ContactPage} />
+          </Switch>
+        </Container>
+      </Router>   
+    );
+}
+
+export default Layout;
