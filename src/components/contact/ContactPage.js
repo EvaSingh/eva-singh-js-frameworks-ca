f195cec20ac490a09a4662d2e08f520acde907aa
Eva Singh <eva.singh@hotmail.com>
2020-12-09 20:13:00 +0100

    Add contact form

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Heading from "../layout/Heading";
+import ContactForm from "./ContactForm";
 
 export function ContactPage() {
     return (
       <>
         <Heading content="Contact"/>
-    
+        <ContactForm></ContactForm>
       </>    
     );
 }
