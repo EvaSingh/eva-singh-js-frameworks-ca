bea6d2d8de0a49f00398afe6318a1d59e353e50b
Eva Singh <eva.singh@hotmail.com>
2020-12-13 20:06:59 +0100

    Add link to GameDetail component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -13,27 +13,27 @@ import ContactPage from "../contact/ContactPage";
 import GameDetail from "../games/GameDetail";
 
 function Layout({children}) {
-    return(
-      <Router>
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="/">React CA</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <NavLink to="/" exact className="nav-link">Home</NavLink>
-                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        <Container>
-          <Switch>
-              <Route path="/" exact component={HomePage} />
-              <Route path="/contact" component={ContactPage} />
-              <Route path="/game/:id" component={GameDetail} />
-          </Switch>
-        </Container>
-      </Router>   
-    );
+  return(
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="/">React CA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                  <NavLink to="/" exact className="nav-link">Home</NavLink>
+                  <NavLink to="/contact" className="nav-link">Contact</NavLink>
+              </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route path="/game/:id" component={GameDetail} />
+        </Switch>
+      </Container>
+    </Router>   
+  );
 }
 
 export default Layout;
