80dc10de910e996ce9f86ff215db4f007ab8c470
Eva Singh <eva.singh@hotmail.com>
2020-12-13 19:59:38 +0100

    Clean up code

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,13 +3,12 @@ import GameList from "../games/GameList";
 import Heading from "../layout/Heading";
 
 export function HomePage() {
-    return (
-      <>
-        <Heading content="Games"/>
-        <GameList></GameList>
-    
-      </>    
-    );
+  return (
+    <>
+      <Heading content="Games"/>
+      <GameList></GameList>
+    </>    
+  );
 }
 
 export default HomePage;
