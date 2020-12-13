8a9202d861223e8fe0f46613076dd720aaa8ea56
Eva Singh <eva.singh@hotmail.com>
2020-12-13 20:09:17 +0100

    Add loading spinner, update and import GameItem and update GameDetail

--- src/components/games/GameItem.js
+++ src/components/games/GameItem.js
@@ -1,26 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 
 function GameItem({ id, name, background_image, released, rating }) {
-    return (
-        <Card>
-          <Card.Img variant="top" src={background_image} />
-          <Card.Body>
-            <Card.Title>{name}</Card.Title>
-            <Card.Text>
-              <b>Released:</b> {released} <br/>
-              <b>Rating:</b> {rating}
-            </Card.Text>
-            <Link to={"game/" + id}>
-              <Button variant="primary" block>
-                View
-              </Button>
-            </Link>
-          </Card.Body>
-        </Card>
-    );
+  return (
+    <Card>
+      <Card.Img variant="top" src={background_image} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          <b>Released:</b> {released} <br/>
+          <b>Rating:</b> {rating}
+        </Card.Text>
+        <Link to={"game/" + id}>
+          <Button variant="primary" block>
+            View
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
 }
+
+GameItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  background_image: PropTypes.string.isRequired,
+  released: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 export default GameItem;
--- src/components/games/GameList.js
+++ src/components/games/GameList.js
@@ -1,12 +1,16 @@
 import React, {  useState, useEffect } from "react";
+import Spinner from "react-bootstrap/Spinner";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BASE_URL } from "../../constants/api";
 import GameItem from "./GameItem";
+import SearchGame from "./SearchGame";
 
 
 export function GameList() {
   const [games, displayGames] = useState([]);
+  const [filteredGames, setFilteredGames] = useState([]);
+  const [loading, setLoading] = useState(true);
     
   useEffect( () => {
     fetch(BASE_URL)
@@ -14,23 +18,49 @@ export function GameList() {
     .then (json => {
       displayGames(json.results);
       console.log(json.results);
-    });
-
-         
+      setFilteredGames(json.results);
+    })
+    .catch(error => console.log(error))
+    .finally(() => setLoading(false));
+          
   },[]);
 
-  return (
-    <Row>
-      {games.map(game => {
-        const {id, name, background_image, released, rating } = game;
+  const filterGames = function(e) {
+    const searchValue = e.target.value.toLowerCase();
+    const filteredArray = games.filter(function(g) {
 
-        return (
-          <Col xs="6" md="4" lg="3">
-            <GameItem id={id} name={name} background_image={background_image} released={released} rating={rating}/>
-          </Col>
-        );
-      })}
-    </Row>
+
+      const lowerCaseName = g.name.toLowerCase();
+
+      if (lowerCaseName.startsWith(searchValue)) {
+        return true;
+      }
+      return false;
+    });
+
+    setFilteredGames(filteredArray);
+  };
+
+  if (loading) {
+		return <Spinner animation="border" className="spinner" />;
+	}
+
+  return (
+    <>
+      <SearchGame handleSEarch={filterGames} />
+
+      <Row>
+        {filteredGames.map(game => {
+          const {id, name, background_image, released, rating } = game;
+
+          return (
+            <Col xs={6} md={4} lg={3} key={id}>
+              <GameItem id={id}  name={name} background_image={background_image} released={released} rating={rating}/>
+            </Col>
+          );
+        })}
+      </Row>
+    </> 
   );
 }
 
