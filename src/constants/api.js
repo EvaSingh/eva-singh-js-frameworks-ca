a43d80bb837636bc26fbcdc641f70982cae9a53b
Eva Singh <eva.singh@hotmail.com>
2020-12-13 19:56:47 +0100

    Update base url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://api.rawg.io/api/games?key=77a4d2a1cf48429c89e28636f101c34e&dates=2019-10-10,2020-10-10&ordering=-added";
+export const BASE_URL = "https://api.rawg.io/api/games";
